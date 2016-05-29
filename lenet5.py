dbae2b92fda81cebcb9602d16d3f7d781c790818
Leman FENG <flm8620@gmail.com>
2016-05-29 18:05:47 +0200

    seperate data

--- lenet5.py
+++ lenet5.py
@@ -13,31 +13,33 @@ import lasagne
 
 def build_lenet5(input_var=None):
     # Written by Leman FENG
-    network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
-                                        input_var=input_var)
+    net = {}
 
-    network = lasagne.layers.Conv2DLayer(
-        network, num_filters=20, filter_size=(5, 5),
+    net['input'] = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
+                                             input_var=input_var)
+
+    net['conv1'] = lasagne.layers.Conv2DLayer(
+        net['input'], num_filters=20, filter_size=(5, 5),
         nonlinearity=lasagne.nonlinearities.rectify,
         W=lasagne.init.GlorotUniform())
 
-    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+    net['pool1'] = lasagne.layers.MaxPool2DLayer(net['conv1'], pool_size=(2, 2))
 
-    network = lasagne.layers.Conv2DLayer(
-        network, num_filters=50, filter_size=(5, 5),
+    net['conv2'] = lasagne.layers.Conv2DLayer(
+        net['pool1'], num_filters=50, filter_size=(5, 5),
         nonlinearity=lasagne.nonlinearities.rectify)
 
-    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
+    net['pool2'] = lasagne.layers.MaxPool2DLayer(net['conv2'], pool_size=(2, 2))
 
-    network = lasagne.layers.DenseLayer(
-        network,
+    net['full'] = lasagne.layers.DenseLayer(
+        net['pool2'],
         num_units=500,
         nonlinearity=lasagne.nonlinearities.rectify)
 
     # And, finally, the 10-unit output layer with 50% dropout on its inputs:
-    network = lasagne.layers.DenseLayer(
-        network,
+    net['output'] = lasagne.layers.DenseLayer(
+        net['full'],
         num_units=10,
         nonlinearity=lasagne.nonlinearities.softmax)
 
-    return network
+    return net['output']
--- train.py
+++ train.py
@@ -90,126 +90,36 @@ def load_dataset():
     return X_train, y_train, X_val, y_val, X_test, y_test
 
 
-# ##################### Build the neural network model #######################
-# This script supports three types of models. For each one, we define a
-# function that takes a Theano variable representing the input and returns
-# the output layer of a neural network model built in Lasagne.
+def seperate_data(data_x, data_y):
+    assert len(data_x) == len(data_y)
+    index1 = []
+    index2 = []
+    for i in range(len(data_y)):
+        if data_y[i] <= 4:
+            index1.append(i)
+        else:
+            index2.append(i)
+    index1 = np.array(index1)
+    index2 = np.array(index2)
+    data_1_x = data_x[index1]
+    data_1_y = data_y[index1]
 
-def build_mlp(input_var=None):
-    # This creates an MLP of two hidden layers of 800 units each, followed by
-    # a softmax output layer of 10 units. It applies 20% dropout to the input
-    # data and 50% dropout to the hidden layers.
+    data_2_x = data_x[index2]
+    data_2_y = data_y[index2]
 
-    # Input layer, specifying the expected input shape of the network
-    # (unspecified batchsize, 1 channel, 28 rows and 28 columns) and
-    # linking it to the given Theano variable `input_var`, if any:
-    l_in = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
-                                     input_var=input_var)
-
-    # Apply 20% dropout to the input data:
-    l_in_drop = lasagne.layers.DropoutLayer(l_in, p=0.2)
-
-    # Add a fully-connected layer of 800 units, using the linear rectifier, and
-    # initializing weights with Glorot's scheme (which is the default anyway):
-    l_hid1 = lasagne.layers.DenseLayer(
-        l_in_drop, num_units=800,
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-
-    # We'll now add dropout of 50%:
-    l_hid1_drop = lasagne.layers.DropoutLayer(l_hid1, p=0.5)
-
-    # Another 800-unit layer:
-    l_hid2 = lasagne.layers.DenseLayer(
-        l_hid1_drop, num_units=800,
-        nonlinearity=lasagne.nonlinearities.rectify)
-
-    # 50% dropout again:
-    l_hid2_drop = lasagne.layers.DropoutLayer(l_hid2, p=0.5)
-
-    # Finally, we'll add the fully-connected output layer, of 10 softmax units:
-    l_out = lasagne.layers.DenseLayer(
-        l_hid2_drop, num_units=10,
-        nonlinearity=lasagne.nonlinearities.softmax)
-
-    # Each layer is linked to its incoming layer(s), so we only need to pass
-    # the output layer to give access to a network in Lasagne:
-    return l_out
+    return data_1_x, data_1_y, data_2_x, data_2_y
 
 
-def build_custom_mlp(input_var=None, depth=2, width=800, drop_input=.2,
-                     drop_hidden=.5):
-    # By default, this creates the same network as `build_mlp`, but it can be
-    # customized with respect to the number and size of hidden layers. This
-    # mostly showcases how creating a network in Python code can be a lot more
-    # flexible than a configuration file. Note that to make the code easier,
-    # all the layers are just called `network` -- there is no need to give them
-    # different names if all we return is the last one we created anyway; we
-    # just used different names above for clarity.
+def load_dataset_seperate(get_first_part=True):
+    X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
 
-    # Input layer and dropout (with shortcut `dropout` for `DropoutLayer`):
-    network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
-                                        input_var=input_var)
-    if drop_input:
-        network = lasagne.layers.dropout(network, p=drop_input)
-    # Hidden layers and dropout:
-    nonlin = lasagne.nonlinearities.rectify
-    for _ in range(depth):
-        network = lasagne.layers.DenseLayer(
-            network, width, nonlinearity=nonlin)
-        if drop_hidden:
-            network = lasagne.layers.dropout(network, p=drop_hidden)
-    # Output layer:
-    softmax = lasagne.nonlinearities.softmax
-    network = lasagne.layers.DenseLayer(network, 10, nonlinearity=softmax)
-    return network
-
-
-def build_cnn(input_var=None):
-    # As a third model, we'll create a CNN of two convolution + pooling stages
-    # and a fully-connected hidden layer in front of the output layer.
-
-    # Input layer, as usual:
-    network = lasagne.layers.InputLayer(shape=(None, 1, 28, 28),
-                                        input_var=input_var)
-    # This time we do not apply input dropout, as it tends to work less well
-    # for convolutional layers.
-
-    # Convolutional layer with 32 kernels of size 5x5. Strided and padded
-    # convolutions are supported as well; see the docstring.
-    network = lasagne.layers.Conv2DLayer(
-        network, num_filters=32, filter_size=(5, 5),
-        nonlinearity=lasagne.nonlinearities.rectify,
-        W=lasagne.init.GlorotUniform())
-    # Expert note: Lasagne provides alternative convolutional layers that
-    # override Theano's choice of which implementation to use; for details
-    # please see http://lasagne.readthedocs.org/en/latest/user/tutorial.html.
-
-    # Max-pooling layer of factor 2 in both dimensions:
-    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
-
-    # Another convolution with 32 5x5 kernels, and another 2x2 pooling:
-    network = lasagne.layers.Conv2DLayer(
-        network, num_filters=32, filter_size=(5, 5),
-        nonlinearity=lasagne.nonlinearities.rectify)
-    network = lasagne.layers.MaxPool2DLayer(network, pool_size=(2, 2))
-
-    # A fully-connected layer of 256 units with 50% dropout on its inputs:
-    network = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(network, p=.5),
-        num_units=256,
-        nonlinearity=lasagne.nonlinearities.rectify)
-
-    # And, finally, the 10-unit output layer with 50% dropout on its inputs:
-    network = lasagne.layers.DenseLayer(
-        lasagne.layers.dropout(network, p=.5),
-        num_units=10,
-        nonlinearity=lasagne.nonlinearities.softmax)
-
-    return network
-
-
-
+    X_train_1, y_train_1, X_train_2, y_train_2 = seperate_data(X_train, y_train)
+    X_val_1, y_val_1, X_val_2, y_val_2 = seperate_data(X_val, y_val)
+    X_test_1, y_test_1, X_test_2, y_test_2 = seperate_data(X_test, y_test)
+    if get_first_part:
+        return X_train_1, y_train_1, X_val_1, y_val_1, X_test_1, y_test_1
+    else:
+        return X_train_2, y_train_2, X_val_2, y_val_2, X_test_2, y_test_2
 
 
 # ############################# Batch iterator ###############################
@@ -237,24 +147,14 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
         yield inputs[excerpt], targets[excerpt]
 
 
-# square loss
-
-def one_hot(x, n):
-    """
-    convert index representation to one-hot representation
-    """
-    x = np.array(x)
-    assert x.ndim == 1
-    return np.eye(n)[x].astype(np.float32)
-
-
 # ############################## Main program ################################
 # Everything else will be handled in our main program now. We could pull out
 # more functions to better separate the code, but it wouldn't make it any
 # easier to read.
 
-def main(model='cifar', num_epochs=20,model_file=None):
+def main(model='cifar', num_epochs=20, model_file=None):
     batch_size = 64
+    seperate = True
     print('batch_size=', batch_size)
     # Load the dataset
     print("Loading data...")
@@ -262,6 +162,15 @@ def main(model='cifar', num_epochs=20,model_file=None):
         X_train, y_train, X_val, y_val, X_test, y_test = get_cifar10()
     else:
         X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
+
+    if seperate:
+        X_train_1, y_train_1, X_train_2, y_train_2 = seperate_data(X_train, y_train)
+        X_val_1, y_val_1, X_val_2, y_val_2 = seperate_data(X_val, y_val)
+        X_test_1, y_test_1, X_test_2, y_test_2 = seperate_data(X_test, y_test)
+        if True:
+            X_train, y_train, X_val, y_val, X_test, y_test = X_train_1, y_train_1, X_val_1, y_val_1, X_test_1, y_test_1
+        else:
+            X_train, y_train, X_val, y_val, X_test, y_test = X_train_2, y_train_2, X_val_2, y_val_2, X_test_2, y_test_2
 
     print(len(X_train), 'train images')
     print(len(X_val), 'val images')
@@ -273,15 +182,7 @@ def main(model='cifar', num_epochs=20,model_file=None):
 
     # Create neural network model (depending on first command line parameter)
     print("Building model and compiling functions...")
-    if model == 'mlp':
-        network = build_mlp(input_var)
-    elif model.startswith('custom_mlp:'):
-        depth, width, drop_in, drop_hid = model.split(':', 1)[1].split(',')
-        network = build_custom_mlp(input_var, int(depth), int(width),
-                                   float(drop_in), float(drop_hid))
-    elif model == 'cnn':
-        network = build_cnn(input_var)
-    elif model == 'lenet':
+    if model == 'lenet':
         network = build_lenet5(input_var)
     elif model == 'cifar':
         net = cifar10_nin.build_model2(input_var)
@@ -294,45 +195,30 @@ def main(model='cifar', num_epochs=20,model_file=None):
             param_values = [f['arr_%d' % i] for i in range(len(f.files))]
         lasagne.layers.set_all_param_values(network, param_values)
 
-    # Create a loss expression for training, i.e., a scalar objective we want
-    # to minimize (for our multi-class problem, it is the cross-entropy loss):
     prediction = lasagne.layers.get_output(network)
     loss = lasagne.objectives.categorical_crossentropy(prediction, target_var)
-    #loss = lasagne.objectives.squared_error(prediction, target_var)
     loss = loss.mean()
-    # We could add some weight decay as well here, see lasagne.regularization.
 
-    # Create update expressions for training, i.e., how to modify the
-    # parameters at each training step. Here, we'll use Stochastic Gradient
-    # Descent (SGD) with Nesterov momentum, but Lasagne offers plenty more.
     params = lasagne.layers.get_all_params(network, trainable=True)
     updates = lasagne.updates.nesterov_momentum(
         loss, params, learning_rate=0.01, momentum=0.9)
 
-    # Create a loss expression for validation/testing. The crucial difference
-    # here is that we do a deterministic forward pass through the network,
-    # disabling dropout layers.
     test_prediction = lasagne.layers.get_output(network, deterministic=True)
-    #test_loss = lasagne.objectives.squared_error(test_prediction, target_var)
     test_loss = lasagne.objectives.categorical_crossentropy(test_prediction, target_var)
     test_loss = test_loss.mean()
-    # As a bonus, also create an expression for the classification accuracy:
+
     test_acc = T.mean(T.eq(T.argmax(test_prediction, axis=1), target_var),
                       dtype=theano.config.floatX)
 
-    # Compile a function performing a training step on a mini-batch (by giving
-    # the updates dictionary) and returning the corresponding training loss:
     train_fn = theano.function([input_var, target_var], loss, updates=updates)
-
-    # Compile a second function computing the validation loss and accuracy:
     val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
 
-    # Finally, launch the training loop.
+
     print("Starting training...")
-    # We iterate over epochs:
+
     for epoch in range(num_epochs):
         time_epoch = time.time()
-        # In each epoch, we do a full pass over the training data:
+
         train_err = 0
         train_batches = 0
         start_time = time.time()
@@ -345,7 +231,7 @@ def main(model='cifar', num_epochs=20,model_file=None):
             train_batches += 1
             print('train batch', train_batches, 'err+=', this_train_err, time.time() - time_batch, 'seconds')
 
-        # And a full pass over the validation data:
+
         val_err = 0
         val_acc = 0
         val_batches = 0
