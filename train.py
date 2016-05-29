76857d429130879f46ead6f84caf93360320977d
Leman FENG <flm8620@gmail.com>
2016-05-29 21:20:35 +0200

    no get cifar

--- train.py
+++ train.py
@@ -23,128 +23,14 @@ import theano
 import theano.tensor as T
 
 import lasagne
-from get_cifar10 import *
 import cifar10_nin
 from lenet5 import *
 import time
+from load_data import *
 
 
-# ################## Download and prepare the MNIST dataset ##################
-# This is just some way of getting the MNIST dataset from an online location
-# and loading it into numpy arrays. It doesn't involve Lasagne at all.
-
-def load_dataset():
-    # We first define a download function, supporting both Python 2 and 3.
-    if sys.version_info[0] == 2:
-        from urllib import urlretrieve
-    else:
-        from urllib.request import urlretrieve
-
-    def download(filename, source='http://yann.lecun.com/exdb/mnist/'):
-        print("Downloading %s" % filename)
-        urlretrieve(source + filename, filename)
-
-    # We then define functions for loading MNIST images and labels.
-    # For convenience, they also download the requested files if needed.
-    import gzip
-
-    def load_mnist_images(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the inputs in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=16)
-        # The inputs are vectors now, we reshape them to monochrome 2D images,
-        # following the shape convention: (examples, channels, rows, columns)
-        data = data.reshape(-1, 1, 28, 28)
-        # The inputs come as bytes, we convert them to float32 in range [0,1].
-        # (Actually to range [0, 255/256], for compatibility to the version
-        # provided at http://deeplearning.net/data/mnist/mnist.pkl.gz.)
-        return data / np.float32(256)
-
-    def load_mnist_labels(filename):
-        if not os.path.exists(filename):
-            download(filename)
-        # Read the labels in Yann LeCun's binary format.
-        with gzip.open(filename, 'rb') as f:
-            data = np.frombuffer(f.read(), np.uint8, offset=8)
-        # The labels are vectors of integers now, that's exactly what we want.
-        return data
-
-    # We can now download and read the training and test set images and labels.
-    X_train = load_mnist_images('train-images-idx3-ubyte.gz')
-    y_train = load_mnist_labels('train-labels-idx1-ubyte.gz')
-    X_test = load_mnist_images('t10k-images-idx3-ubyte.gz')
-    y_test = load_mnist_labels('t10k-labels-idx1-ubyte.gz')
-
-    # We reserve the last 10000 training examples for validation.
-    X_train, X_val = X_train[:-10000], X_train[-10000:]
-    y_train, y_val = y_train[:-10000], y_train[-10000:]
-
-    print(len(X_train), 'train images')
-    print(len(X_val), 'validation images')
-    print(len(X_test), 'test images')
-
-    # We just return all the arrays in order, as expected in main().
-    # (It doesn't matter how we do this as long as we can read them again.)
-    return X_train, y_train, X_val, y_val, X_test, y_test
 
 
-def seperate_data(data_x, data_y):
-    assert len(data_x) == len(data_y)
-    index1 = []
-    index2 = []
-    for i in range(len(data_y)):
-        if data_y[i] <= 4:
-            index1.append(i)
-        else:
-            index2.append(i)
-    index1 = np.array(index1)
-    index2 = np.array(index2)
-    data_1_x = data_x[index1]
-    data_1_y = data_y[index1]
-
-    data_2_x = data_x[index2]
-    data_2_y = data_y[index2]
-
-    return data_1_x, data_1_y, data_2_x, data_2_y
-
-
-def load_dataset_seperate(get_first_part=True):
-    X_train, y_train, X_val, y_val, X_test, y_test = load_dataset()
-
-    X_train_1, y_train_1, X_train_2, y_train_2 = seperate_data(X_train, y_train)
-    X_val_1, y_val_1, X_val_2, y_val_2 = seperate_data(X_val, y_val)
-    X_test_1, y_test_1, X_test_2, y_test_2 = seperate_data(X_test, y_test)
-    if get_first_part:
-        return X_train_1, y_train_1, X_val_1, y_val_1, X_test_1, y_test_1
-    else:
-        return X_train_2, y_train_2, X_val_2, y_val_2, X_test_2, y_test_2
-
-
-# ############################# Batch iterator ###############################
-# This is just a simple helper function iterating over training data in
-# mini-batches of a particular size, optionally in random order. It assumes
-# data is available as numpy arrays. For big datasets, you could load numpy
-# arrays as memory-mapped files (np.load(..., mmap_mode='r')), or write your
-# own custom data iteration function. For small datasets, you can also copy
-# them to GPU at once for slightly improved performance. This would involve
-# several changes in the main program, though, and is not demonstrated here.
-# Notice that this function returns only mini-batches of size `batchsize`.
-# If the size of the data is not a multiple of `batchsize`, it will not
-# return the last (remaining) mini-batch.
-
-def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
-    assert len(inputs) == len(targets)
-    if shuffle:
-        indices = np.arange(len(inputs))
-        np.random.shuffle(indices)
-    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
-        if shuffle:
-            excerpt = indices[start_idx:start_idx + batchsize]
-        else:
-            excerpt = slice(start_idx, start_idx + batchsize)
-        yield inputs[excerpt], targets[excerpt]
 
 
 # ############################## Main program ################################
@@ -155,6 +41,11 @@ def iterate_minibatches(inputs, targets, batchsize, shuffle=False):
 def main(model='lenet', num_epochs=20, model_file=None):
     batch_size = 64
     seperate = True
+    if seperate:
+        nOutput=5
+    else:
+        nOutput=10
+    load_first_part = True
     print('batch_size=', batch_size)
     # Load the dataset
     print("Loading data...")
@@ -169,7 +60,7 @@ def main(model='lenet', num_epochs=20, model_file=None):
         X_train_1, y_train_1, X_train_2, y_train_2 = seperate_data(X_train, y_train)
         X_val_1, y_val_1, X_val_2, y_val_2 = seperate_data(X_val, y_val)
         X_test_1, y_test_1, X_test_2, y_test_2 = seperate_data(X_test, y_test)
-        if True:
+        if load_first_part:
             X_train, y_train, X_val, y_val, X_test, y_test = X_train_1, y_train_1, X_val_1, y_val_1, X_test_1, y_test_1
         else:
             X_train, y_train, X_val, y_val, X_test, y_test = X_train_2, y_train_2, X_val_2, y_val_2, X_test_2, y_test_2
@@ -185,9 +76,11 @@ def main(model='lenet', num_epochs=20, model_file=None):
     # Create neural network model (depending on first command line parameter)
     print("Building model and compiling functions...")
     if model == 'lenet':
-        network = build_lenet5(input_var)
+        net = build_lenet5(input_var,nOutput)
+        network = net['output']
     elif model == 'cifar':
-        net = cifar10_nin.build_model2(input_var)
+        # pass
+        net = cifar10_nin.build_model2(input_var,nOutput)
         network = net['output']
     else:
         print("Unrecognized model type %r." % model)
@@ -215,7 +108,6 @@ def main(model='lenet', num_epochs=20, model_file=None):
     train_fn = theano.function([input_var, target_var], loss, updates=updates)
     val_fn = theano.function([input_var, target_var], [test_loss, test_acc])
 
-
     print("Starting training...")
 
     for epoch in range(num_epochs):
@@ -232,7 +124,6 @@ def main(model='lenet', num_epochs=20, model_file=None):
             train_err += this_train_err
             train_batches += 1
             print('train batch', train_batches, 'err+=', this_train_err, time.time() - time_batch, 'seconds')
-
 
         val_err = 0
         val_acc = 0
