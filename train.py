e63628c578345be6eab3ea01b43303f93c902da4
Leman FENG <flm8620@gmail.com>
2016-05-30 03:11:33 +0200

    log debug

--- train.py
+++ train.py
@@ -43,7 +43,7 @@ def get_all_params_from_layers(layers, unwrap_shared=True, **tags):
 # Everything else will be handled in our main program now. We could pull out
 # more functions to better separate the code, but it wouldn't make it any
 # easier to read.
-def log_and_print(logfile,*arg):
+def log_and_print(*arg, logfile):
     with open(logfile, 'a') as f:
         f.write(*arg)
         print(*arg)
